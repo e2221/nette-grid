fee084ab0178ecef33446e3187f6184d96f9bbdc
krall <luke.kral@seznam.cz>
2020-11-11 22:03:00 +0100

    init

--- src/Document/Templates/TableTemplate.php
+++ src/Document/Templates/TableTemplate.php
@@ -7,5 +7,6 @@ namespace e2221\NetteGrid\Document\Templates;
 
 class TableTemplate extends BaseTemplate
 {
+    protected ?string $elName = 'table';
     public string $defaultClass = 'table table-sm';
 }
