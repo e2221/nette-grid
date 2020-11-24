a0618f6a5549d387440b6e8e2e037e08655b2299
krall <luke.kral@seznam.cz>
2020-11-24 14:30:05 +0100

    update

--- src/Document/Templates/Cols/DataColTemplate.php
+++ src/Document/Templates/Cols/DataColTemplate.php
@@ -9,4 +9,13 @@ use e2221\NetteGrid\Document\Templates\BaseColTemplate;
 class DataColTemplate extends BaseColTemplate
 {
     protected ?string $elementName='td';
+
+    /**
+     * @param null $columnName
+     */
+    public function prepareElement($columnName=null): void
+    {
+        $this->addDataAttribute('column', $columnName);
+        parent::prepareElement();
+    }
 }
