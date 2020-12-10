1561b978a4dc79e0e1edb92104225f26f243e207
krall <luke.kral@seznam.cz>
2020-12-10 10:30:13 +0100

    Row action delete

--- src/Actions/RowActions/RowActionDelete.php
+++ src/Actions/RowActions/RowActionDelete.php
@@ -10,7 +10,7 @@ class RowActionDelete extends RowAction
     public function beforeRender(): void
     {
         $this->addIconElement('fas fa-trash-alt', [], true);
-        $this->addDataAttribute('dynamic-remove', sprintf('%s-%s-%s', $this->netteGrid->getUniqueId(), $this->netteGrid::SNIPPET_TBODY, $this->primary));
+        $this->addDataAttribute('dynamic-remove', sprintf('#%s-%s-%s', $this->netteGrid->getUniqueId(), $this->netteGrid::SNIPPET_TBODY, $this->primary));
         parent::beforeRender();
     }
 }
