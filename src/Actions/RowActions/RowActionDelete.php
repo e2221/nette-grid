e3c73e7b7b9bc4112e386b2c3d36f2f142384e98
krall <luke.kral@seznam.cz>
2021-01-08 09:31:50 +0100

    Row action delete - add trash icon in construct to possible redefine

--- src/Actions/RowActions/RowActionDelete.php
+++ src/Actions/RowActions/RowActionDelete.php
@@ -3,13 +3,20 @@ declare(strict_types=1);
 
 namespace e2221\NetteGrid\Actions\RowAction;
 
+use e2221\NetteGrid\NetteGrid;
+
 class RowActionDelete extends RowAction
 {
     public string $class='btn-danger';
 
+    public function __construct(NetteGrid $netteGrid, string $name, ?string $title = null)
+    {
+        parent::__construct($netteGrid, $name, $title);
+        $this->addIconElement('far fa-trash-alt', [], true);
+    }
+
     public function beforeRender(): void
     {
-        $this->addIconElement('far fa-trash-alt', [], true);
         $this->addDataAttribute('data-history', "false");
         $this->addDataAttribute('dynamic-remove', sprintf('#%s-%s', $this->netteGrid->getSnippetId($this->netteGrid::SNIPPET_TBODY), $this->primary));
         parent::beforeRender();
