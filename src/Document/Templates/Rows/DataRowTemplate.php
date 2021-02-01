5e8ecd289dc40751f26dcbd067f96ab06b6f2e6b
krall <luke.kral@seznam.cz>
2021-02-01 12:16:36 +0100

    Uset method setStyleElementCallback => use DocumentTemplate->setDataRowCallback(DataRowTemplate $t, $row): DataRowTemplate;

--- src/Document/Templates/Rows/DataRowTemplate.php
+++ src/Document/Templates/Rows/DataRowTemplate.php
@@ -21,9 +21,6 @@ class DataRowTemplate extends BaseTemplate
     /** @var null|callable Draggable helper text callback: function($row, $primary):?string */
     protected $draggableHelperCallback=null;
 
-    /** @var null|callable Style element callback: function(DataRowTemplate $dataRowTemplate, $row, $primary):DataRowTemplate */
-    protected $styleElementCallback=null;
-
     public function __construct(DocumentTemplate $documentTemplate)
     {
         parent::__construct();
@@ -48,33 +45,9 @@ class DataRowTemplate extends BaseTemplate
         parent::prepareElement();
     }
 
-    public function beforeRender(): void
-    {
-        parent::beforeRender();
-
-        if(!is_null($this->row) && !is_null($this->primary))
-        {
-            $styleElementCallback = $this->styleElementCallback;
-            if(is_callable($styleElementCallback))
-                $styleElementCallback($this, $this->row, $this->primary);
-        }
-    }
-
-
     public function endTemplate(): DocumentTemplate
     {
         return $this->documentTemplate;
-    }
-
-    /**
-     * Set style element callback
-     * @param callable|null $styleElementCallback
-     * @return DataRowTemplate
-     */
-    public function setStyleElementCallback(?callable $styleElementCallback): self
-    {
-        $this->styleElementCallback = $styleElementCallback;
-        return $this;
     }
 
     /**
